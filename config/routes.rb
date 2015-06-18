812f037506814f4e40194ea92ce96a56febbecaa
Thomas Wodarek <thomas@thomaswodarek.com>
2015-06-17 21:45:08 -0400

    Hello World achieved

--- config/routes.rb
+++ config/routes.rb
@@ -53,4 +53,8 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+	root 'hello_world#show'
+	get '/hello_world', to: 'hello_world#show'
+
+
 end
